cb1752f219126129d8649ea90f7941835b9d6008
JakeOlst <jstainthorpe123@gmail.com>
2025-01-17 19:40:52 +0000

    Added function for processing a LinkedList alphabetically

--- QueueCollection.java
+++ QueueCollection.java
@@ -1,18 +1,30 @@
-import java.util.LinkedList;
 import java.util.Queue;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class QueueCollection {
     public static void main(String[] args) {
-        // Queues
-
-        // Instantiates a Queue reference with an LinkedList implementation
         Queue<String> line = new LinkedList<>();
         line.add("Mike");
         line.add("Isabel");
         line.add("Jenny");
 
-        for(String name: line) {
+        for (String name : line) {
             System.out.println(name);
+        }
+
+        processAlphabetically(line);
+    }
+
+    public static void processAlphabetically(Queue<String> queue) {
+        Queue<String> alphabeticalQueue = new PriorityQueue<>();
+        for (String name : queue) {
+            alphabeticalQueue.add(name);
+        }
+
+        while (alphabeticalQueue.peek() != null) {
+            String headElement = alphabeticalQueue.remove();
+            System.out.println("Processing: " + headElement);
         }
     }
 }
