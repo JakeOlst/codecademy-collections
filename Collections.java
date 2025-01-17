7cef38f3564a9396e82117fb10c250ba7b5c2574
JakeOlst <jstainthorpe123@gmail.com>
2025-01-17 19:28:18 +0000

    Added configuration for List Collections

--- Collections.java
+++ Collections.java
@@ -3,6 +3,18 @@ import java.util.ArrayList;
 
 public class Collections {
     public static void main(String[] args) {
-        
+        // Lists
+
+        // Instantiates a List reference with an ArrayList implementation
+        List<String> stringList = new ArrayList<>();
+
+        stringList.add("Hello");
+        stringList.add("World");
+        stringList.add("!");
+
+        // Iterates through the list.
+        for (String element : stringList) {
+            System.out.println(element);
+        }
     }
 }
