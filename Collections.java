110f768e0fd2c79b0b093630fd93e115f8390fc5
JakeOlst <jstainthorpe123@gmail.com>
2025-01-17 19:29:39 +0000

    Added configuration for Set Collections

--- Collections.java
+++ Collections.java
@@ -1,4 +1,6 @@
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.ArrayList;
 
 public class Collections {
@@ -15,6 +17,20 @@ public class Collections {
         // Iterates through the list.
         for (String element : stringList) {
             System.out.println(element);
+        }
+
+        // Sets
+
+        // Instantiates a Set reference with an ArrayList implementation
+        Set<Integer> sortedSet = new TreeSet<>();
+        sortedSet.add(3);
+        sortedSet.add(7);
+        sortedSet.add(27);
+        sortedSet.add(7);
+
+        // Iterates through the set
+        for (Integer element : sortedSet) {
+        System.out.println(element);
         }
     }
 }
