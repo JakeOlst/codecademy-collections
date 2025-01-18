36d5b06cf46e473f5a0b05b925a11aac2b8ac7c8
JakeOlst <jstainthorpe123@gmail.com>
2025-01-18 17:00:54 +0000

    Implemented base code for Map Collections.

--- Collections.java
+++ Collections.java
@@ -1,10 +1,11 @@
 
 public class Collections {
     public static void main(String[] args) {
-       ListCollection.main(null); // Lists
-       SetCollection.main(null); // Sets
+       ListCollection.main(null); // List
+       SetCollection.main(null); // Set
        QueueCollection.main(null); // Queue
        DequeCollection.main(null); // Deque
-       MyCollection.main(null); // Collection Type
+       MyCollection.main(null); // Collection 
+       MapCollection.main(null); // Map
     }
 }
---
+++ MapCollection.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Random;
+public class MapCollection {
+    public static void main(String[] args) {
+        List<Integer> myInts = new ArrayList<>();
+        Random random = new Random();
+    
+        for(int i =0; i < 20; i++) {
+          myInts.add(random.nextInt(5));
+        }
+    
+        System.out.println("myInts: " + myInts);
+    
+        Map<Integer, Integer> intCount = countNumbers(myInts);
+        for(Map.Entry<Integer, Integer> entry: intCount.entrySet()) {
+          System.out.println("Integer: "+ entry.getKey()+" appears: "+ entry.getValue());
+        }
+      }
+    
+      public static Map<Integer, Integer> countNumbers(List<Integer> list) {
+        return null;
+      }
+}
