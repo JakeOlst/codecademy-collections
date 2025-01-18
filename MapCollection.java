321bc31cd395039797ee19192d96e47420acd1fb
JakeOlst <jstainthorpe123@gmail.com>
2025-01-18 17:15:02 +0000

    Created function to count instances of each integer in a list, returning a HashMap.

--- MapCollection.java
+++ MapCollection.java
@@ -3,24 +3,35 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Random;
+
 public class MapCollection {
     public static void main(String[] args) {
         List<Integer> myInts = new ArrayList<>();
         Random random = new Random();
-    
-        for(int i =0; i < 20; i++) {
-          myInts.add(random.nextInt(5));
+
+        for (int i = 0; i < 20; i++) {
+            myInts.add(random.nextInt(5));
         }
-    
+
         System.out.println("myInts: " + myInts);
-    
+
         Map<Integer, Integer> intCount = countNumbers(myInts);
-        for(Map.Entry<Integer, Integer> entry: intCount.entrySet()) {
-          System.out.println("Integer: "+ entry.getKey()+" appears: "+ entry.getValue());
+        for (Map.Entry<Integer, Integer> entry : intCount.entrySet()) {
+            System.out.println("Integer: " + entry.getKey() + " appears: " + entry.getValue());
         }
-      }
-    
-      public static Map<Integer, Integer> countNumbers(List<Integer> list) {
-        return null;
-      }
+    }
+
+    public static Map<Integer, Integer> countNumbers(List<Integer> list) {
+        Map<Integer, Integer> intCount = new HashMap<>();
+        for (int i : list) {
+            if (intCount.get(i) == null) {
+                intCount.put(i, 1);
+            } else {
+                int count = intCount.get(i);
+                count++;
+                intCount.put(i, count);
+            }
+        }
+        return intCount;
+    }
 }
